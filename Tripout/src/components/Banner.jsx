a38fd8c698d916c46647b183349105a31c4193c5
CodeGallantX <johnayobami77@proton.me>
2024-11-05 14:18:53 +0100

    ...

--- Tripout/src/components/Banner.jsx
+++ Tripout/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Banner = ({ page }) => {
   return (
     <div className="relative w-full h-[350px] flex flex-col justify-center items-center px-6 md:px-10 xl:px-44">
-      <img src="/Banner.png" className=" object-cover w-full"/>
+      <img src="/banner.png" className=" object-cover w-full"/>
       <div className="absolute z-10 mt-10 left-1/2 -translate-x-1/2 ">
         <h2 className="text-5xl md:text-7xl font-semibold mb-4 font-serif">{page.title}</h2>
 
