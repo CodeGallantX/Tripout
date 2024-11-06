5d59c04683a0806cf66ce870d29c39b6499c91d7
CodeGallantX <johnayobami77@proton.me>
2024-11-06 19:35:44 +0100

    ....

--- Tripout/src/components/DestinationIntro.jsx
+++ Tripout/src/components/DestinationIntro.jsx
@@ -36,7 +36,7 @@ const DestinationIntro = () => {
                             &nbsp; Wishlist
                         </p>
                         <p className="leading-relaxed text-sm bg-[#FDE9D1] text-[#F7921E] hover:bg-orange-200 cursor-pointer rounded py-1 px-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right inline-block fill-[#F7921E]" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-box-arrow-up-right inline-block fill-[#F7921E]" viewBox="0 0 16 16">
                                 <path fillRule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5" />
                                 <path fillRule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z" />
                             </svg>
